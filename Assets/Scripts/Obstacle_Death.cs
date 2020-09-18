181b08fa78a2eb3f69837446cd545af553b0f33d
Ryan Erickson <rsericks@email.unc.edu>
2020-09-18 15:12:58 -0400

    qa

---
+++ Assets/Scripts/Obstacle_Death.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Hosting;
+using UnityEngine;
+
+public class Obstacle_Death : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        audioSource.Play();
+        ApplicationActivator.LoadLevel(ApplicationActivator.loadedLevel);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
