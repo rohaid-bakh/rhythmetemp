643f09465098ce5fe7ea269442ac22039ca41f2e
unknown <rohaidb1999@gmail.com>
2020-09-24 19:08:02 -0400

    fixed death script

--- Assets/Scripts/MCRun.cs
+++ Assets/Scripts/MCRun.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class MCRun : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody2D rigidbody2d;
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,5 +20,9 @@ public class MCRun : MonoBehaviour
        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
        position.y = position.y + 3.0f * vertical * Time.deltaTime;
        transform.position = position;
+       if (Input.GetKeyDown(KeyCode.UpArrow)) {
+           float jumpVelocity = 10f;
+           rigidbody2d.velocity = Vector2.up * jumpVelocity;
+       }
     }
 }
--- Assets/Scripts/Obstacle_Death.cs
+++ Assets/Scripts/Obstacle_Death.cs
@@ -6,27 +6,45 @@ using UnityEngine.SceneManagement;
 public class Obstacle_Death : MonoBehaviour
 {
     AudioSource audioSource;
-    
+    bool m_play = true;
+    bool m_toggle = true;
+    int counter = 0;
     // Start is called before the first frame update
     void Start()
     {
+         Debug.Log("Test 0");
+  
         audioSource = GetComponent<AudioSource>();
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Death")
-        {
-            audioSource.Play();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+              audioSource.Play();
         
     }
+
+   //add boolean to toggle sound ?? 
 
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+    Debug.Log(audioSource.time);
+      Debug.Log("Test 1");
+       if (m_toggle == true) {
+         audioSource.Stop();
+         m_toggle = false;
+         m_play = true;
+       }
+       if (m_play == true && m_toggle == false) {
+         audioSource.Play();
+         m_play = false;
+       }
+
     }
+
+      void OnCollisionEnter2D(Collision2D col) {
+        //Resets players to original position
+        if(col.gameObject.tag == "Death"){
+        Debug.Log("Test 1");
+        transform.position = new Vector3(-13, 5, 0);
+        m_toggle = true;
+        }
+    }
+        
 }
